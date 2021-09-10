9bc476feb6adeb42fca67555a6aa6a5a48eab22f
Austin Nguyen <austinzn@ad.unc.edu>
2021-09-10 16:28:06 -0400

    ex03 complete

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,25 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730466197"
+
+word: str = input("Enter a word: ")
+letter: str = ""
+dupe: str = ""
+bank: int = 0
+i: int = 0
+n: int = 0
+
+
+while i < len(word):
+    letter = word[i]
+    while n < len(word):
+        if letter == word[n]:
+            bank = bank + 1
+        n = n + 1
+    n = 0
+    i = i + 1
+if bank > len(word):
+    dupe = "True"
+else:
+    dupe = "False"
+print("Found duplicate: " + dupe)
