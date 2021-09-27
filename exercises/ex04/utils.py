22c360ea0a73236eb54a41df88f2bfc62024dfdd
Austin Nguyen <austinzn@ad.unc.edu>
2021-09-27 11:39:24 -0400

    ex04 done

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -3,5 +3,40 @@
 __author__ = "730466197"
 
 
-def all(x:list[int], y:int) -> bool:
-    while i < len.x:
+def all(integers: list[int], search: int) -> bool:
+    i: int = 0
+    x: int = 0
+    if len(integers) > 0:
+        while i < len(integers):
+            if integers[i] == search:
+                x += 1
+            i += 1
+        if x == len(integers):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def is_equal(integers_one: list[int], integers_two: list[int]) -> bool:
+    if len(integers_one) == len(integers_two):
+        i: int = 0
+        x: int = 0
+        while i < len(integers_one):
+            if integers_one[i] == integers_two[i]:
+                x += 1
+            i += 1
+        if x == len(integers_one):
+            return True
+    return False
+
+
+def max(integers: list[int]) -> int:
+    i: int = 0
+    x: int = integers[0]
+    while i < len(integers):
+        if integers[i] > x:
+            x = integers[i]
+        i += 1
+    return x
