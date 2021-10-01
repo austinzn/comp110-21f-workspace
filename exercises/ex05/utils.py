f8e0f5559956071ad2618575522d072f56180aff
Austin Nguyen <austinzn@ad.unc.edu>
2021-10-01 13:29:15 -0400

    ex05 progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,35 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730466197"
 
 # Define your functions below
+def only_evens(xs: list[int]) -> list[int]:
+    """Returns only the even numbers of a list of numbers."""
+    i: int = 0
+    count: list[int] = list()
+    while i < len(xs):
+        if xs[i] % 2 == 0:
+            count.append(xs[i])
+        i += 1
+    return count
+
+
+def sub(xs: list[int], start: int, end: int) -> list[int]:
+    """Generates a subset of the given list within the set parameters."""
+    i: int = 0
+    subset: list[int] = list()
+    while i < len(xs):
+        if start <= i < end:
+            subset.append(xs[i])
+        i += 1
+    return subset
+
+
+def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Generates a list comprised of the first list followed by the second."""
+    i: int = 0
+    new: list[int] = xs
+    while i < len(ys):
+        new.append(ys[i])
+        i += 1
+    return new
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,16 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730466197"
+
+
+def test_only_evens_regular() -> None:
+    a_list: list[int] = [1, 2, 3]
+    only_evens(a_list)
+
+
+def test_only_evens_blank() -> None:
+    a_list: list[int] = list()
+    only_evens(a_list) 
